3742b42a06ebc024078361346776aa3bb8118312
Bruno <bruno.candeias@indicium.ai>
2026-02-26 13:58:52 +0000

    aa

--- aws_cdk/app.py
+++ aws_cdk/app.py
@@ -27,5 +27,8 @@ from migration_cdk.aws_cdk_stack import AwsCdkStack
 #app.synth()
 
 app = cdk.App()
-AwsCdkStack(app,"testing-11")
+AwsCdkStack(app,"testing-11",
+            env=cdk.Environment(account='647746917454_AWSAdministratorAccess', 
+                        region='eu-west-2'))
+            
 app.synth()
