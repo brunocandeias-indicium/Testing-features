b289348c542dbc167a61773be2f436ccf62e5663
Bruno <bruno.candeias@indicium.ai>
2026-02-26 16:10:11 +0000

    aaa

--- aws_cdk/migration_cdk/aws_cdk_stack.py
+++ aws_cdk/migration_cdk/aws_cdk_stack.py
@@ -2,13 +2,19 @@ import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_ssm as ssm
+from aws_cdk import aws_events as event
 
 class AwsCdkStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        s3.Bucket(self,id = "Bucket_raw" ,bucket_name="sales-migration-raw-bc2")
-        s3.Bucket(self,id = "Bucket_staged" ,bucket_name="sales-migration-staged-bc2")
+        raw_bucket = s3.Bucket(self,id = "Bucket_raw" ,bucket_name="sales-migration-raw-bc2")
+        staging_bucket = s3.Bucket(self,id = "Bucket_staged" ,bucket_name="sales-migration-staged-bc2")
 
+        event_rule_donefile = event.Rule(self, 
+                                         'event_rule_donefile', 
+                                         event_pattern = event.EventPattern(source=[raw_bucket]))
 
+        #ssm_raw = ssm.StringParameter(self,"RawBucketNameParam", raw_bucket.bucket_name)
+        
